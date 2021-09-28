37715e91d40e4f84c766e707c23a9d232380038f
ttaha <mustafatahacilbas@gmail.com>
2021-09-28 14:01:31 +0300

    Initial commit

---
+++ src/ogrenciBilgileri/Main.java
@@ -0,0 +1,16 @@
+package ogrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //  sysout ctrl space
+        System.out.println("Öðrenci Bilgileri");
+		System.out.println("Mustafa Taha");
+		System.out.println("kickboks yapmayý severs");
+		System.out.println("Çilbaþ");//println bayram
+		System.out.println("Mustafa Taha"+" "+"Çilbaþ");
+		System.out.println("Yaþ :"+16);
+	}
+
+}
